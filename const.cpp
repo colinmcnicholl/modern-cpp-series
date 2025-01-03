fb783ff1e0e0442da42faedef7cb9b8aaeff93c7
Colin McNicholl <colin.mcnicholl@gmail.com>
2025-01-03 15:28:12 +0000

    Add files via upload

---
+++ const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  const float PI = 3.141526;
+  // constexpr int expression = 3+6+8;
+  std::cout << PI << std::endl;
+  // PI = -42;
+  std::cout << PI << std::endl;
+
+  return 0;
+}
